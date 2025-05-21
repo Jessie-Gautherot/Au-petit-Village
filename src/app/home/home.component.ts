9ee129a38d9f1fc4fb5fd623df4fb3837fb2cef5
Jessie Gautherot <jessie.gautherot@gmail.com>
2025-05-21 20:35:06 +0200

    add service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsService } from '../produits.service';
+import { Produit } from '../produit.model';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: Produit[] = [];
+  errorMessage = '';
 
+  constructor(private productsService: ProductsService) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.products = await this.productsService.getProduits();
+    } catch (error) {
+      console.error('Erreur lors du chargement des produits:', error);
+      this.errorMessage = 'Impossible de charger les produits.';
+    }
+  }
 }
